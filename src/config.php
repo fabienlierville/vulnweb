939160e6b07746cfab6d7577287ec9867e2696d7
fabienlierville <53614625+fabienlierville@users.noreply.github.com>
2025-12-18 14:23:55 +0100

    Add files via upload

---
+++ src/config.php
@@ -0,0 +1,7 @@
+<?php
+$aws_access_key = "AKIAIMADEUPKEY123456"; 
+$aws_access_key2 = "AKIAIMADEUPKEY12345678"; 
+$aws_access_key3 = "AKIAIMADEUPKEY654321"; 
+$aws_secret_key = "z8s9d8s7d6f5g4h3j2k1l0m9n8b7v6c5x4z3a2s1";
+
+?>
